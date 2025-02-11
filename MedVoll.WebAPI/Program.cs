8c52d5ef3e7964c82c2c6b8cb8d336f799c49e3c
marcelooliveira <mclricardo@gmail.com>
2025-02-10 22:46:30 -0300

    video2.4-validando-token-gerado

--- MedVoll.WebAPI/Program.cs
+++ MedVoll.WebAPI/Program.cs
@@ -34,7 +34,18 @@ builder.Services.AddTransient<IConsultaService, ConsultaService>();
 //tokenjwtservice
 builder.Services.AddScoped<TokenJWTService>();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+    opt => opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidAudience = builder.Configuration["JWTTokenConfiguration:Audience"],
+        ValidIssuer = builder.Configuration["JWTTokenConfiguration:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JWTKey:key"]!)),
+    });
 
 builder.Services.ConfigureSwagger();
 
